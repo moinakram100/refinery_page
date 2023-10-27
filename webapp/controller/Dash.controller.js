9304618635cecc2aba6e27ccc83401980f44a69a
Mohin Khan <mohin.khan@ingenxtec.com>
2023-10-27 05:51:34 +0000

    modify

--- webapp/controller/Dash.controller.js
+++ webapp/controller/Dash.controller.js
@@ -1,22 +1,24 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,JSONModel) {
+    function (Controller,JSONModel,MessageToast) {
         "use strict";
 
         return Controller.extend("refinerydash.controller.Dash", {
             onInit: function () {
-              
+                
             },
-            formatCSS: function(bSpecial) {
-                return bSpecial ? "special-item" : "normal-item";
-            },
-            onClick : ()=>{
-                alert("hello")
+        
+            onClick : (oEvent)=>{
+                var oClickedNode = oEvent.getSource();
+                var sTitle = oClickedNode.getTitle();
+                //  MessageToast.show("Clicked Node Title: " + sTitle);
+                MessageToast.show(" Title: " + sTitle);
             }
            
         });
